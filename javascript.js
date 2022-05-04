7832b4d30536b1d980d25cac2f124ddeeed1b24d
mercurialbuddha <44732717+mercurialbuddha@users.noreply.github.com>
2022-05-04 15:38:43 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,5 @@
-//javascript.js
+const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+const d = new Date();
+let day = days[d.getDay()];
+document.getElementById("demo").innerHTML = day;
